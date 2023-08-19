8bfac4fdf9810d9ec167bd71a65f6834c8298b66
Edsel Clarin <edsel.clarin@yahoo.com>
2023-08-19 09:54:49 -0700

    Add project files.

---
+++ MonadTest/Monads/Result.cs
@@ -0,0 +1,62 @@
+﻿namespace MonadTest.Monads
+{
+    /// <summary>
+    /// Either monad pattern which allows action to be taken on the value of 
+    /// either the success or failure case.
+    /// </summary>
+    public class Result<TVal, TErr>
+    {
+        private readonly TVal val_;
+        private readonly TErr err_;
+        private readonly bool isSuccess_;
+
+        private Result(TVal val)
+        {
+            val_ = val;
+            err_ = default(TErr);
+            isSuccess_ = true;
+        }
+
+        private Result(TErr err)
+        {
+            val_ = default(TVal);
+            err_ = err;
+            isSuccess_ = false;
+        }
+
+        public static Result<TVal, TErr> Success(TVal val) => new Result<TVal, TErr>(val);
+        public static Result<TVal, TErr> Failure(TErr err) => new Result<TVal, TErr>(err);
+
+        /// <summary>
+        /// Processes either the value or error depending on isSuccess and returns the new type T.
+        /// </summary>
+        public T Match<T>(Func<TVal, T> successFunc, Func<TErr, T> failureFunc)
+        {
+            return isSuccess_ ? successFunc(val_) : failureFunc(err_);
+        }
+
+        /// <summary>
+        /// Action to execute on successful state.
+        /// </summary>
+        public Result<TVal, TErr> ApplyOnSuccess(Action<TVal> successAction)
+        {
+            if (isSuccess_) 
+            { 
+                successAction(val_); 
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Action to execute on failed state.
+        /// </summary>
+        public Result<TVal, TErr> ApplyOnFailure(Action<TErr> failureAction)
+        {
+            if (!isSuccess_)
+            {
+                failureAction(err_);
+            }
+            return this;
+        }
+    }
+}
---
+++ MonadTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using MonadTest.Monads;
+
+public static class Program
+{
+    public static void Main()
+    {
+        CalculateRatio(2, 4).PrintResult();
+        CalculateRatio(2, 0).PrintResult();
+        CalculateRatio(3, 9).PrintResult();
+
+        // Output:
+        //
+        // Result: 50 %
+        // Error: Cannot divide by zero
+        // Result: 33 %
+    }
+
+    private static void PrintResult(this Result<decimal, string> result)
+    {
+        result
+            .Match<Result<decimal, string>>(
+                val => Result<decimal, string>.Success(val * 100m), // percentage
+                err => Result<decimal, string>.Failure(err))
+            .ApplyOnSuccess(val => Console.WriteLine($"Result: {val:0}%"))
+            .ApplyOnFailure(err => Console.WriteLine($"Error: {err}"));
+    }
+
+    public static Result<decimal, string> CalculateRatio(int part, int total)
+    {
+        // NOTE: Handles error without the need to throw an exception.
+        return total == 0 ?
+            Result<decimal, string>.Failure("Cannot divide by zero") :
+            Result<decimal, string>.Success((decimal)part / (decimal)total);
+    }
+}
